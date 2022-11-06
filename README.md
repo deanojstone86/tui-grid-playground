2af02ebf32260d6035d695995b75d924cd520208
deanojstone86 <50617844+deanojstone86@users.noreply.github.com>
2022-11-06 15:00:10 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tui-grid-playground
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pjkh3w)
---
+++ index.js
@@ -0,0 +1,101 @@
+import Grid from 'tui-grid';
+import 'tui-pagination/dist/tui-pagination.css';
+const data = [
+  {
+    id: '10012',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    id: '10013',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    id: '10014',
+    city: 'London',
+    country: 'England',
+  },
+  {
+    id: '10015',
+    city: 'Ljubljana',
+    country: 'Slovenia',
+  },
+  {
+    id: '10016',
+    city: 'Reykjavik',
+    country: 'Iceland',
+  },
+  {
+    id: '10012',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    id: '10013',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    id: '10014',
+    city: 'London',
+    country: 'England',
+  },
+  {
+    id: '10015',
+    city: 'Ljubljana',
+    country: 'Slovenia',
+  },
+  {
+    id: '10016',
+    city: 'Reykjavik',
+    country: 'Iceland',
+  },
+  {
+    id: '10012',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    id: '10013',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    id: '10014',
+    city: 'London',
+    country: 'England',
+  },
+  {
+    id: '10015',
+    city: 'Ljubljana',
+    country: 'Slovenia',
+  },
+  {
+    id: '10016',
+    city: 'Reykjavik',
+    country: 'Iceland',
+  },
+];
+const grid = new Grid({
+  el: document.getElementById('grid'),
+  data: data,
+  pageOptions: {
+    useClient: true,
+    perPage: 6,
+  },
+  columns: [
+    {
+      header: 'id',
+      name: 'id',
+    },
+    {
+      header: 'city',
+      name: 'city',
+    },
+    {
+      header: 'country',
+      name: 'country',
+    },
+  ],
+});
